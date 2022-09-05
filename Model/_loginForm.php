797f596011b01708018e5fe4c692b06958426686
Jams <Jamsjam971@hotmail.com>
2022-09-05 16:24:02 -0400

    loginForm

---
+++ Model/_loginForm.php
@@ -0,0 +1,19 @@
+
+
+    <div id="formContainer">
+
+        <div class="logInput" >
+            <label for="mail">
+                E-mail
+            </label>
+            <input type="text" name="mail" id="mail">
+        </div>
+        
+        <div class="logInput" >
+            <label for="mdp">
+                Mot de passe
+            </label>
+            <input type="text" name="mdp" id="mdp">
+        </div>
+        
+    </div>
