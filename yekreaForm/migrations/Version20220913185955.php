cb1faf1313513acf4a3dc11f163d26ee9daefcc3
Claudia <claudia.aaron@hotmail.fr>
2022-09-13 15:11:44 -0400

    feat/entity
    creation de l'entité devis + relation avec command

---
+++ yekreaForm/migrations/Version20220913185955.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220913185955 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE devis (id INT AUTO_INCREMENT NOT NULL, num_devis INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE command DROP devis');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE devis');
+        $this->addSql('ALTER TABLE command ADD devis INT DEFAULT NULL');
+    }
+}
