f114661a5d05070dd703d5c6d39f88d59a0db537
Claudia <claudia.aaron@hotmail.fr>
2022-09-14 15:11:18 -0400

    feat/devis

--- yekreaForm/src/Controller/ValidateController.php
+++ yekreaForm/src/Controller/ValidateController.php
@@ -14,23 +14,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ValidateController extends AbstractController
 {
-    /**
+    /** A la validation de la commande, cette route permet:
+     * - la création du numéro de devis dans la table DEVIS
+     * - l'insertion de la "date de validation" dans la table COMMANDE
+     * 
      * @Route("admin/validate/{id}", name="app_validate")
      */
+
     public function index( Command $command, CommandRepository $commandRepository, DevisRepository $devisRepository ): Response
     {   
-        // création du numéro de devis à la validation du bouton
+        
         $devi = new Devis();
 
+        // fonction time -1900 pour créer le num du devis 
         $numDevis= time()-1900;
+        
         $devi->setnumDevis($numDevis);
-
         $devi->setCommand($command);
 
-        // insert dans la table la date de validation de la commande
+        // insert dans la table la date de validation de la commande dans la BDD
         $command->setValidationDate(new DateTimeImmutable("now"));
 
-        // ajoute en BDD si l'id n'existe pas et le modifie si l'ID existe
+        // Si l'ID n'existe pas l'ajoute en BDD 
+        // le modifie si l'ID existe
         $commandRepository->add($command, true);
         $devisRepository->add($devi, true);
         
