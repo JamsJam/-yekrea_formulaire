537d03479bffd90f34a6c81a3101d15d9d7bbe3f
Jams <Jamsjam971@hotmail.com>
2022-09-21 23:28:34 -0400

    Statistique general

--- yekreaForm/src/Controller/DashboardController.php
+++ yekreaForm/src/Controller/DashboardController.php
@@ -9,51 +9,78 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Command;
+use App\Repository\DevisRepository;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="app_dashboard")
      */
-    public function index(CommandRepository $commandRepository, UserRepository $userRepository): Response
+    public function index(CommandRepository $commandRepository, DevisRepository $devisRepository, UserRepository $userRepository): Response
     {
-        // ***********************************Recuperation du premier vendeur
+        
+        // ***********************************Recuperation des 4 dernieres commandes
+
+        $lastCommandes = $commandRepository->findby([],['id'=> 'DESC'], 4);
+
+
+
+        // *********************************** Recuperation du premier vendeur
+
         $users = $userRepository->findAll();
         $resultat = [];
+        $resultat1 = [];
         foreach ($users as $user) {
-            if($user->getRoles() != ['ROLE_USER']){
+            if($user->getRoleInt() != 3 ){
 
                 $id = $user->getId();
                 $nombreDeCommande = count ($commandRepository->findBy(['user'=> $id])) ;
                 $table = [$user->getNom().' '.$user->getPrenom(), $id, $nombreDeCommande ];
                 array_push($resultat, $table);
+
+                // *********************************** Recuperation du vendeur le plus rentable
+
+
+                $nombreDeCommandeValide = ( $commandRepository->findBy(['user'=> $id, 'isValidated'=> true])) ;
+                // if($commande->isValidated() == true)
+                if($nombreDeCommande){
+                    $prix = 0;
+                    foreach( $nombreDeCommandeValide as $commande){
+                        if($commande->getDevis()){
+
+                            if($commande->getDevis()->getStatus() == "accepted"){
+                                $prix += $commande->getDevis()->getPrixFinal();
+                                $table1 = [$user->getNom().' '.$user->getPrenom() , $prix ];
+                                array_push($resultat1, $table1);
+                            }
+                        }
+                    }
+                    
+                }
+                
+                
             }
         }
+        
         $columnVente = array_column($resultat, 2);
         array_multisort($columnVente, SORT_DESC, $resultat);
-
-        // ****************************************recuperation du vendeur le plus rentable
-        $commandes =  $commandRepository->findall();
-        $prixCommande = [];
-        foreach ($commandes as $commande) {
-            if($commande->getDevis() !== null){
-
-                if($commande->getDevis()->getPrixFinal() !== null ){
-                    $prixFinal = $commande->getDevis()->getPrixFinal();
-                    $table2 = [$commande->getUser()->getNom().' '.$commande->getUser()->getPrenom(), $prixFinal];
-                    array_push($prixCommande,$table2);
-                }  
-            }
-        }
-        // dd($prixCommande);
+        
+        $columnPrix = array_column($resultat1, 1);
+        array_multisort($columnPrix, SORT_DESC, $resultat1);
+        
+        
 
 
 
 
         return $this->render('dashboard/index.html.twig', [
-            
-            'lastCommandes' =>  $commandRepository->findby([],['id'=> 'DESC'], 4),
-            'premierVender' => $resultat[0],
+            // ****************************************recuperation du vendeur le plus rentable
+            "devisAccepted"     =>  count($devisRepository->findby(['status' => 'accepted'])),
+            "devisAborted"      => count($devisRepository->findby(['status' => 'aborted'])),
+            "devisPending"      => count($devisRepository->findby(['status' => 'pending'])),
+            'lastCommandes'     =>  $lastCommandes,
+            'premierVender'     => $resultat[0],
+            'vendeurRentable'   => $resultat1[0],
         ]);
     }
 }
